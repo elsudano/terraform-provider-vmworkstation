d323d0b680de2d71fd3aea797851386b62ec0e93
Carlos <cdelatorre@correo.ugr.es>
2021-05-30 20:01:32 +0200

    First functional version of Makefile in Windows, just the skeleton

--- Makefile.ps1
+++ Makefile.ps1
@@ -1,3 +1,7 @@
+[string]$OS = "windows"
+[string]$ARCH = "amd64"
+[string]$SIGNFILES = "publish_files/"
+
 Function help {
     [array]$HELPS = Select-String -Path .\Makefile.ps1 -Pattern "^(Function?) (?<fun>\w+) (.+##) (?<help>.+)"
     $HELPS | ForEach-Object {
@@ -15,6 +19,7 @@ Function compress { ## With this function we comppress the files in one, and the
 }
 
 Function build { ## Build the binary of the module
+    prepare_environment
     Write-Host "we did Build"
 }
 
@@ -29,18 +34,18 @@ Function publish { ## This option prepare the zip files to publishing in Terrafo
     compress
     Write-Host "we did Publish"
 }
-
+ 
 Function prepare_environment {
     [string]$CONFIGFILE = ".\binary_config.ini"
-    [array]$CONFIGURATIONS = Get-Content $CONFIGFILE
-
-    foreach ($CONFI in $CONFIGURATIONS) {
-        [string]$name, [string]$value = $CONFI -split "=", 2
-        Write-Host "Valor de la variable $name : $value"
+    [array]$OPTIONS = Select-String -Path $CONFIGFILE -Pattern "^(?<key>\w+)=(?<value>.+$)"
+    $OPTIONS | ForEach-Object {
+        $key, $value = $_.Matches[0].Groups['key','value'].Value
+        Set-Variable -Name $key -Value $value
     }
-    # $PREFIX = terraform-provider
-    # $NAME = vmworkstation
-    # $VERSION = 0.2.2
+    [string]$BINARY = "$PREFIX-$NAME_v$VERSION"
+    [string]$ZIPFILE = "$PREFIX-$NAME_$VERSION_$OS_$ARCH.zip"
+    [string]$SHAFILE = "$PREFIX-$NAME_$VERSION_SHA256SUMS"
+    Write-Host $PREFIX $NAME $VERSION $BINARY $ZIPFILE $SHAFILE
 }
 # --------------------------
 #       Menu
