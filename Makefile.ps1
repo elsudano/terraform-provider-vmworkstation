3b10ee46c7897fa3fa59c98303e72de7bcc80230
Carlos <cdelatorre@correo.ugr.es>
2021-05-30 14:52:13 +0200

    Add new file to config binary and first approach to Makefile (powershell) in Windows

--- Makefile.ps1
+++ Makefile.ps1
@@ -1,3 +1,10 @@
-$args
-Clear-Host
-Write-Host "Los argumentos son: $($args.count)"
+[string]$CONFIGFILE = ".\binary_config.ini"
+[array]$CONFIGURATIONS = Get-Content $CONFIGFILE
+
+foreach ($CONFI in $CONFIGURATIONS) {
+    [string]$name, [string]$value = $CONFI -split "=", 2
+    Write-Host "Valor de la variable $name : $value"
+}
+# $PREFIX = terraform-provider
+# $NAME = vmworkstation
+# $VERSION = 0.2.2
