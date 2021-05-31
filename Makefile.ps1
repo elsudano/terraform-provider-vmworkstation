b50dea939592ee30647310be4bce69175049a3bd
Carlos <cdelatorre@correo.ugr.es>
2021-05-31 19:52:33 +0200

    Fix variable names, and add teh funcionality of HASH

--- Makefile.ps1
+++ Makefile.ps1
@@ -1,12 +1,12 @@
-[string]$OS = "windows"
-[string]$ARCH = "amd64"
-[string]$SIGNFILES = "publish_files/"
+[string]${OS} = "windows"
+[string]${ARCH} = "amd64"
+[string]${SIGNFILES} = "publish_files/"
 
 Function help {
-    [array]$HELPS = Select-String -Path .\Makefile.ps1 -Pattern "^(Function?) (?<fun>\w+) (.+##) (?<help>.+)"
-    $HELPS | ForEach-Object {
-        $var, $help = $_.Matches[0].Groups['fun','help'].Value
-        Write-Host "$var : `t$help"
+    [array]${HELPS} = Select-String -Path .\Makefile.ps1 -Pattern "^(Function?) (?<fun>\w+) (.+##) (?<help>.+)"
+    ${HELPS} | ForEach-Object {
+        ${var}, ${help} = $_.Matches[0].Groups['fun','help'].Value
+        Write-Host "${var} : `t${help}"
     }
 }
 
@@ -25,27 +25,29 @@ Function build { ## Build the binary of the module
 
 Function install { ## Copy binary to the project and det SHA256SUM in the config of project, NOTE: Just for Dev. environment for both Terraform 0.12 and 0.13_beta2
     build
-    Write-Host "we did Install"
+    Write-Host "When you to be develop a provider, is better use the ~/.terraformrc file"
 }
 
 Function publish { ## This option prepare the zip files to publishing in Terraform Registry
     clean
     install 
     compress
-    Write-Host "we did Publish"
+    [string]${HASH} = Get-FileHash -Path ${ZIPFILE} | Select-Object Hash
+    ${HASH} | Out-File -FilePath ${SHAFILE}
+    Write-Host "we did Publish and the Hash is: ${HASH}"
 }
  
 Function prepare_environment {
-    [string]$CONFIGFILE = ".\binary_config.ini"
-    [array]$OPTIONS = Select-String -Path $CONFIGFILE -Pattern "^(?<key>\w+)=(?<value>.+$)"
-    $OPTIONS | ForEach-Object {
-        $key, $value = $_.Matches[0].Groups['key','value'].Value
-        Set-Variable -Name $key -Value $value
+    [string]${CONFIGFILE} = ".\binary_config.ini"
+    [array]${OPTIONS} = Select-String -Path $CONFIGFILE -Pattern "^(?<key>\w+)=(?<value>.+$)"
+    ${OPTIONS} | ForEach-Object {
+        ${key}, ${value} = $_.Matches[0].Groups['key','value'].Value
+        Set-Variable -Name ${key} -Value ${value}
     }
-    [string]$BINARY = "$PREFIX-$NAME_v$VERSION"
-    [string]$ZIPFILE = "$PREFIX-$NAME_$VERSION_$OS_$ARCH.zip"
-    [string]$SHAFILE = "$PREFIX-$NAME_$VERSION_SHA256SUMS"
-    Write-Host $PREFIX $NAME $VERSION $BINARY $ZIPFILE $SHAFILE
+    [string]${BINARY} = "${PREFIX}-${NAME}_v${VERSION}"
+    [string]${ZIPFILE} = "${PREFIX}-${NAME}_${VERSION}_${OS}_${ARCH}.zip"
+    [string]${SHAFILE} = "${PREFIX}-${NAME}_${VERSION}_SHA256SUMS"
+    Write-Host ${PREFIX} ${NAME} ${VERSION} ${BINARY} ${ZIPFILE} ${SHAFILE}
 }
 # --------------------------
 #       Menu
