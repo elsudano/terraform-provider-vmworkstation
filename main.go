17d30f63f85e28db5716a83b62119ebcd3241a05
Carlos Ariel de la Torre <carlos.ariel.de.la.torre@gmail.com>
2024-02-25 13:04:03 +0100

    chore: we have did some improvements in the code

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"log"
+
 	"github.com/elsudano/terraform-provider-vmworkstation/vmworkstation"
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
 )
 
 func main() {
+	// Remove any date and time prefix in log package function output to
+	// prevent duplicate timestamp and incorrect log level setting
+	log.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
+
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: vmworkstation.Provider,
 	})
