01e3abb9a3a561ac019108ecc59489837e47038c
Carlos Ariel de la Torre <carlos.ariel.de.la.torre@gmail.com>
2025-06-01 22:32:15 +0200

    chore: We have cleaned the main.go file of the provider

--- main.go
+++ main.go
@@ -20,20 +20,17 @@ var (
 
 func main() {
 	var debug bool
-
-	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like develop")
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
 	flag.Parse()
-
 	opts := providerserver.ServeOpts{
+		ProtocolVersion: 6,
 		// TODO: Update this string with the published name of your provider.
 		// Also update the tfplugindocs generate command to either remove the
 		// -provider-name flag or set its value to the updated provider name.
 		Address: "registry.terraform.io/elsudano/vmworkstation",
 		Debug:   debug,
 	}
-
 	err := providerserver.Serve(context.Background(), vmworkstation.New(version), opts)
-
 	if err != nil {
 		log.Fatal(err.Error())
 	}
